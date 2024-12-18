8efe2293fa70ffeda2420f57d097dfcf20877cd5
dungddzai <128717617+dungddzai@users.noreply.github.com>
2024-12-18 17:55:08 +0700

    d

--- CNPM_QuanLyChuyenBay/CNPM_QuanLyChuyenBay/App_Start/RouteConfig.cs
+++ CNPM_QuanLyChuyenBay/CNPM_QuanLyChuyenBay/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace CNPM_QuanLyChuyenBay
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "DatVe", action = "TimKiemChuyenBay", id = UrlParameter.Optional }
             );
         }
     }
